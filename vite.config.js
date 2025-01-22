f282eee8e0e1d62519b155e942c0dce2acdcf384
wbo_dbaselga <wbo_dbaselga@ext.laligatech.com>
2025-01-22 11:43:22 +0100

    Publicar en gitHub pages

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,23 @@ import react from '@vitejs/plugin-react'
 // import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    target: 'esnext',
-  },
-  resolve: {
-    alias: {
-      '@': '/src',
+export default defineConfig(({ command}) => {
+  const config = {
+    plugins: [react()],
+    base: "/",
+    build: {
+      target: 'esnext',
     },
-  },
+    resolve: {
+      alias: {
+        '@': '/src',
+      },
+    },
+  }
+
+  if (command !== "serve") {
+    config.base = "/removebgtest/";
+  }
+
+  return config;
 })
